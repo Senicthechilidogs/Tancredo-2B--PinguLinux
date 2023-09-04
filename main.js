c4e8b112448d32e7e2ac3be267640e11e7869552
Gibran Arellano <114931589+Senicthechilidogs@users.noreply.github.com>
2023-09-04 10:49:55 -0300

    aula-04/09/23
    aula-04/09/23

---
+++ main.js
@@ -0,0 +1,17 @@
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+function tocaSomSplash () {
+    document.querySelector('#som_tecla_splash').play();
+}
+    document.querySelector('.tecla_splash').onclick = tocaSomSplash;
+
+function tocaSomPuff () {
+    document.querySelector('#som_tecla_puff').play();
+
+}
+
+document.querySelector('.tecla_puff').onclick = tocaSomPuff;
